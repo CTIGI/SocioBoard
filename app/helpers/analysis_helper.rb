b98fd38c57e6d3ea0adc30f2cce621def4f79c2d
Leo Caminha <leoncio.sobreira@gmail.com>
2016-03-21 17:43:00 -0300

    FIX

--- app/helpers/analysis_helper.rb
+++ app/helpers/analysis_helper.rb
@@ -1,12 +1,12 @@
 module AnalysisHelper
   def show_table_unconformities_value(age_value, min_age, max_age, measure_value)
     td_class = ""
-    
+
     if measure_value > 0
       if age_value.between?(min_age, max_age)
-        td_class = "table-blue-rowspan"
+        td_class = "table-blue-rowspan text-center"
       else
-        td_class = "table-red-rowspan"
+        td_class = "table-red-rowspan text-center"
       end
     end
 

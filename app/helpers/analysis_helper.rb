3f15494920ab9bd338729040a66e5f91166e533d
Schwarz <schwarzeneggeral@gmail.com>
2016-03-21 15:10:53 -0300

    [(WIP) #115620645] unconformities improvements

---
+++ app/helpers/analysis_helper.rb
@@ -0,0 +1,17 @@
+module AnalysisHelper
+  def show_table_unconformities_value(age_value, min_age, max_age, measure_value)
+    td_class = ""
+    
+    if measure_value > 0
+      if age_value.between?(min_age, max_age)
+        td_class = "table-blue-rowspan"
+      else
+        td_class = "table-red-rowspan"
+      end
+    end
+
+    content_tag(:td, class: td_class) do
+      concat( measure_value ) if measure_value > 0
+    end
+  end
+end

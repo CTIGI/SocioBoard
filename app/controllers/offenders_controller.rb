bc64688536bf7c302e9c863d58852b1bc7a8ac25
Leo Caminha <leoncio.sobreira@gmail.com>
2016-03-28 15:33:26 -0300

    FIXED

--- app/controllers/offenders_controller.rb
+++ app/controllers/offenders_controller.rb
@@ -2,15 +2,15 @@ class OffendersController < ApplicationController
   include Concerns::OffendersControllerConcern
 
   def index
-    if params.include?(:search) || !params[:q].present?
+    if params.include?(:export_pdf)
+      redirect_to action: :generate_pdf, format: :pdf
+    elsif params.include?(:export_sheet)
+      redirect_to action: :generate_sheet, format: :xlsx
+    else
       @q = Offender.ransack(session[:query_search])
       @result = @q.result
       @offenders = @result.page(params[:page])
       respond_with(@offenders)
-    elsif params.include?(:export_pdf)
-      redirect_to action: :generate_pdf, format: :pdf
-    elsif params.include?(:export_sheet)
-      redirect_to action: :generate_sheet, format: :xlsx
     end
   end
 end
